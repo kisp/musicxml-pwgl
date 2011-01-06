1db0846b307be47455709a04211aaeb4ca4f44e0
Kilian Sprotte <kilian.sprotte@gmail.com>
2011-01-06 12:23:30 +0100

    update Copyright

--- scripts/cruise-build.rb
+++ scripts/cruise-build.rb
@@ -1,6 +1,6 @@
 # This file is part of MusicXML-PWGL.
 
-# Copyright (c) 2010, Kilian Sprotte. All rights reserved.
+# Copyright (c) 2010 - 2011, Kilian Sprotte. All rights reserved.
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
